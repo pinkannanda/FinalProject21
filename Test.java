6b70ef6002f931cc525cc96095fa59a769a5a35e
septyaandhita-stack <septyaandhita@gmail.com>
2025-10-08 08:58:40 +0700

    dhita ngubah

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Ini folder aslinya Pinkan");
+        System.out.println("Ini dhita ngubah filenya Pinkan");
     }
     
 }
